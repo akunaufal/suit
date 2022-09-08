d7249759083bac392428528c7fb1166471262d92
Naufal <farajnaufalachmad@gmail.com>
2022-09-09 02:05:09 +0700

    Belajar Javascript

---
+++ script.js
@@ -0,0 +1,48 @@
+//ulang
+let ulang = true;
+	let skorP = 0;
+	let skorC = 0;
+while (ulang) {
+	//player
+	const player = prompt('Cobalah kau pilih\norang, semut, atau gajah?');
+	if(player == null) {
+		alert('coba main dulu sekali');
+	} else {
+	//computer
+	const compRandom = Math.random();
+	let comp = '';
+
+	//others variable
+	let wrongInput = 'pilih salah satu lah, jangan ngasal';
+	let hasil = '';
+
+	//Scoring
+
+	//random
+	if(comp < 0.33) {
+		comp = 'orang';
+	} else if(comp < 0.66) {
+		comp = 'semut';
+	} else {
+		comp = 'gajah';
+	}
+	//rules
+	if(comp == player) {
+		hasil = 'KALIAN SERI!!!';
+	} else if(player == 'orang') {
+		hasil = ( comp == 'gajah' ? 'KAU KALAH!!!' +skorC++: 'KAU MENANG!!!' + skorP++);
+	} else if(player == 'semut') {
+		hasil = (comp == 'orang' ? 'KAU KALAH!!!' +skorC++: 'KAU MENANG!!!' + skorP++);
+	} else if(player == 'gajah') {
+		hasil = (comp == 'semut' ? 'KAU KALAH!!!' +skorC++: 'KAU MENANG!!!' + skorP++);
+	} else {
+		hasil = wrongInput;
+	}
+	//result
+
+	alert('Kau milih                = ' + player + '\nKomputer milih    = ' + comp + '\n' + hasil + '\n\nSkor' + '\nKau  ' + skorP + '  ---  ' + skorC + ' Komputer');
+
+	ulang = confirm('Lagi kah?'); }
+}
+
+alert('Arigato'); 
